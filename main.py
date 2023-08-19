05d099d460509cc6f1feed41b921e1f7d9a7bd65
morales <juan.morales2004@hotmail.com>
2023-08-18 22:41:28 -0500

    first commit

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 
-url = "https://www.paginasamarillas.com.co/servicios/hoteles"
+url = "https://www.yelp.com/search?find_desc=Delivery&find_loc=San+Francisco%2C+CA&attrs=RestaurantsDelivery&start=0"
 driver = webdriver.Chrome()
 driver.get(url)
 driver.maximize_window() 
 
-data = {'name': [], 'address': [], 'description': [], 'phone': []}
+data = {'name': [], 'description': [], 'address': [], 'typeOfFood' : [], 'information' : []}
 
-elements = driver.find_elements(By.CSS_SELECTOR, ".advertise.Advertise_advertise__wUfbF.high.Advertise_high__pUp9O")    
+elements = driver.find_elements(By.CSS_SELECTOR, ".padding-t3__09f24__TMrIW.padding-r3__09f24__eaF7p.padding-b3__09f24__S8R2d.padding-l3__09f24__IOjKY.border-color--default__09f24__NPAKY")    
 for item in elements:
-    data["name"].append(item.find_element(By.CSS_SELECTOR, ".title.Advertise_title__9FF4M>a").text)
-    data["address"].append(item.find_element(By.CSS_SELECTOR, ".address.Advertise_address__Nb86U>div>b").text)
-    data["description"].append(item.find_element(By.CSS_SELECTOR, ".text.Advertise_text__e0Gx2>p").text)
-    data["phone"].append(item.find_element(By.CSS_SELECTOR, ".phone.Advertise_phone__HDuX5").text)
+
+    data["name"].append(item.find_element(By.CSS_SELECTOR, ".css-19v1rkv").text)
+
+    data["description"].append(item.find_element(By.CSS_SELECTOR, ".arrange-unit__09f24__rqHTg.arrange-unit-fill__09f24__CUubG.border-color--default__09f24__NPAKY>.").text)
+
+    data["address"].append(item.find_element(By.CSS_SELECTOR, ".css-dzq7l1 span:nth-child(2)").text)
+
+    data["typeOfFood"].append(item.find_element(By.CSS_SELECTOR, ".css-1iacd0p>.css-11bijt4").text)
+
+    data["information"].append(item.find_element(By.CSS_SELECTOR, ".label__09f24__hNq6C.display--inline__09f24__c6N_k.border-color--default__09f24__NPAKY>p>.raw__09f24__T4Ezm").text)
     
 driver.quit()
 
